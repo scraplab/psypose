a9d20997fdec4522ca16aaf6fb3808475871b1ea
Landry S. Bulls <bulls409@gmail.com>
2021-05-17 19:32:16 -0400

    model files are now hosted by us on GDrive

--- psypose/prepare_data.sh
+++ psypose/prepare_data.sh
@@ -4,20 +4,17 @@ cd model_weights
 gdown "https://drive.google.com/uc?id=1eyE-IIHpkswHhYnPXX3HByrZrSiXk00g"
 gdown "https://drive.google.com/uc?id=1AkYZmHJ_LsyQYsML6k72A662-AdKwxsv"
 
-cd ../../MEVA/scripts
+cd ../../MEVA
 mkdir -p data
 cd data
-gdown "https://drive.google.com/uc?id=14CjsrGqzZeQ_H76ZYCiWwX4e-VAyxt_Q"
+gdown "https://drive.google.com/uc?export=download&id=1l5pUrV5ReapGd9uaBXrGsJ9eMQcOEqmD"
 unzip meva_data.zip
 rm meva_data.zip
 cd ..
 mkdir -p $HOME/.torch/models/
-mv data/meva_data/yolov3.weights $HOME/.torch/models/
 mkdir -p results/meva/vae_rec_2/models
 mkdir -p results/meva/train_meva_2/
 
-rm results/meva/train_meva_2/model_best.pth.tar
-rm results/meva/vae_rec_2/models/model_1000.p
-
 cp data/meva_data/model_1000.p results/meva/vae_rec_2/models/
-mv data/meva_data/model_best.pth.tar results/meva/train_meva_2/model_best.pth.tar
+mv data/meva_data/spin_model_checkpoint.pth.tar results/meva/train_meva_2/spin_model_checkpoint.pth.tar
+
