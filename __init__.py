c04c70d19b142d4c5020649135cb465877193537
Landry Bulls <bulls409@gmail.com>
2021-05-18 16:54:19 -0400

    removed pyproject.toml for now

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 13:34:17 2021
+
+@author: Landry Bulls
+"""
+
+from __future__ import absolute_import
+
+__author__ = """Landry Bulls """
+__email__ = 'Landry.S.Bulls@Dartmouth.edu'
+__all__ = ['extract', 'data', 'utils', 'display', 'regressors']
+
+from .data import pose
+from .extract import annotate
+
