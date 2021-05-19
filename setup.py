ead1114160868513b704c01e14346c2ff67abf9f
Landry Bulls <bulls409@gmail.com>
2021-05-19 14:09:54 -0400

    reverting to original setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from pathlib import Path
 
 from setuptools import setup, find_packages
 
-
-#requirements_path = Path(__file__).resolve().parent.joinpath('requirements.txt')
-#requirements = requirements_path.read_text().splitlines()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(name='psypose',
       version='0.0.1',
@@ -16,31 +14,5 @@ setup(name='psypose',
       author_email='Landry.S.Bulls@dartmouth.edu',
       license='MIT',
       packages=['psypose'],
-      install_requires=['py-feat',
-             'face-recognition',
-             'nibabel',
-             'networkx',
-             'scikit-learn',
-             'pyyaml',
-             'future',
-             'tqdm',
-             'scipy',
-             'chumpy',
-             'numba',
-             'yacs',
-             'joblib',
-             'trimesh',
-             'pyrender',
-             'pytube',
-             'filterpy',
-             'h5py',
-             'scikit-image',
-             'opencv-python>=3.4.11.43',
-             'torch',
-             'torchvision',
-             'tensorboard==2.0',
-             'git+https://github.com/ZhengyiLuo/smplx.git',
-             'git+https://github.com/mkocabas/yolov3-pytorch.git',
-             'git+https://github.com/pytorch/vision.git',
-             'git+https://github.com/mkocabas/multi-person-tracker.git'],
+      install_requires=requirements
       zip_safe=False)
