44cf477479c8e985725aba932d32276c01b92b99
Landry Bulls <bulls409@gmail.com>
2021-05-20 14:18:55 -0400

    test print

--- src/psypose/__init__.py
+++ src/psypose/__init__.py
@@ -16,6 +16,7 @@ import zipfile36 as zipfile
 import shutil
 
 return_dir = os.getcwd()
+print(return_dir)
 os.chdir('models/model_weights')
 # face encoding models:
 print('Downloading facenet weights...')
