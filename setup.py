63a25a8bf037c18237c0624206f6a88c22753f47
Landry Bulls <bulls409@gmail.com>
2021-05-21 15:31:30 -0400

    trying to put .yml files in setup

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+meva_cfg_files = ['MEVA/meva/cfg/train_meva_1.yml',
+                  'MEVA/meva/cfg/train_meva_2.yml',
+                  'MEVA/meva/cfg/train_meva.yml',
+                  'MEVA/meva/cfg/train_vae.yml',
+                  'MEVA/meva/cfg/vae_rec_1.yml',
+                  'MEVA/meva/cfg/vae_rec_2.yml']
+
 setup(name='psypose',
       version='0.0.1',
       description='Tools for processing pose information in psychological research.',
@@ -14,5 +21,6 @@ setup(name='psypose',
       author_email='Landry.S.Bulls@dartmouth.edu',
       license='MIT',
       packages=find_packages(),
+
       install_requires=requirements,
       zip_safe=False)
