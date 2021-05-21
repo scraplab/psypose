0bb0f933b025d6c88539cbb234a944b7d9483007
Landry Bulls <bulls409@gmail.com>
2021-05-21 15:45:14 -0400

    fixing MANIFEST

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-meva_cfg_files = ['psypose/MEVA/meva/cfg/train_meva_1.yml',
-                  'psypose/MEVA/meva/cfg/train_meva_2.yml',
-                  'psypose/MEVA/meva/cfg/train_meva.yml',
-                  'psypose/MEVA/meva/cfg/train_vae.yml',
-                  'psypose/MEVA/meva/cfg/vae_rec_1.yml',
-                  'psypose/MEVA/meva/cfg/vae_rec_2.yml']
-
 setup(name='psypose',
       version='0.0.1',
       description='Tools for processing pose information in psychological research.',
@@ -21,6 +14,6 @@ setup(name='psypose',
       author_email='Landry.S.Bulls@dartmouth.edu',
       license='MIT',
       packages=find_packages(),
-      package_data=meva_cfg_files,
+      include_package_data=True,
       install_requires=requirements,
       zip_safe=False)
