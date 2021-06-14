725b433a6acf9a5cffdb0e57775c64765b29412e
Landry Bulls <bulls409@gmail.com>
2021-06-14 17:03:20 -0400

    fixed indent

--- src/psypose/extract.py
+++ src/psypose/extract.py
@@ -76,7 +76,7 @@ def annotate(pose, face_box_model='mtcnn', au_model='rf', face_id_model='deepfac
             pose.face_data.to_csv(output_path+'/'+pose.vid_name+'/psypose_faces.csv')
          joblib.dump(pose.pose_data, os.path.join(output_path+'/'+pose.vid_name+'/psypose_bodies.pkl'))
 
-    print('Finished annotation for file: ', t.vid_name)
+     print('Finished annotation for file: ', t.vid_name)
      
      
 
