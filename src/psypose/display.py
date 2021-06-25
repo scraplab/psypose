4c4675920546741b78b64d6303c01646ee6e2ffb
Landry Bulls <bulls409@gmail.com>
2021-06-25 14:11:00 -0400

    fixing person presence viz

--- src/psypose/display.py
+++ src/psypose/display.py
@@ -304,17 +304,17 @@ def collapse(pickle_array):
 
 def show_tracks(pose):
     pkl_arr = pkl_to_array(pose)
-    pkl_arr = collapse(pkl_arr)
+    #pkl_arr = collapse(pkl_arr)
 
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, dpi=200, figsize=(10, 5), sharex=True)
 
-    ax1.imshow(pickle_array, aspect='auto', interpolation='none', cmap='copper')
+    ax1.imshow(pkl_arr, aspect='auto', interpolation='none', cmap='copper')
     for track in range(pickle_array.shape[0]):    
         ax1.hlines(track-0.5, xmin=0, xmax=pickle_array.shape[1], color='white')
     ax1.set(ylabel='Track ID')
     ax1.set(title='Person presence per track')
         
-    ax2.plot(collapse(pickle_array))
+    ax2.plot(collapse(pkl_arr))
     ax2.set(ylabel='# People', xlabel='Frame ID', title='Number of people on screen')
 #show_tracks(pkl_arr)
 #video_cap = cv2.VideoCapture(video)
