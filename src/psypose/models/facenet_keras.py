b1b55caf8ffe4e9521d59be8ff901c2146d0c9e0
Landry Bulls <bulls409@gmail.com>
2021-06-29 16:40:38 -0400

    silencing annoying output

--- src/psypose/models/facenet_keras.py
+++ src/psypose/models/facenet_keras.py
@@ -5,7 +5,7 @@ Created on Tue Feb  2 17:40:20 2021
 
 @author: f004swn
 """
-
+from contextlib import redirect_stdout # or try redirect_stderr
 from keras.models import load_model
 import numpy as np
 from PIL import Image
@@ -21,15 +21,16 @@ model = load_model(model_path, compile=False)
 #facenet expects that the pixel values be standardized 
 
 def encode(face_array):
-    # this takes the RGB form image array that is output my the PLIERS loop
-    face = Image.fromarray(face_array)
-    face = face.resize((160,160))
-    face = np.asarray(face)
-    face = face.astype('float32')
-    mean, std = face.mean(), face.std()
-    face = (face - mean) / std
-    samples = np.expand_dims(face, axis=0)
-    yhat = model.predict(samples)
-    encoding = yhat[0]
-    return encoding
+    with open(os.devnull) as devnull, redirect_stdout(devnull):
+        # this takes the RGB form image array that is output my the PLIERS loop
+        face = Image.fromarray(face_array)
+        face = face.resize((160,160))
+        face = np.asarray(face)
+        face = face.astype('float32')
+        mean, std = face.mean(), face.std()
+        face = (face - mean) / std
+        samples = np.expand_dims(face, axis=0)
+        yhat = model.predict(samples)
+        encoding = yhat[0]
+        return encoding
 
